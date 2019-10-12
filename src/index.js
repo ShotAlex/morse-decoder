9045833b700233c57391ba70fe950eda08362fc2
ShotAlex <shot.aleks@gmail.com>
2019-10-13 02:18:56 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,28 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let strOneChar = expr.match(/.{10}/g);
+    let arrDotAndDash = [];
+    let arrWords = [];
+
+    for (let i = 0; i < strOneChar.length; i++) {
+        let q = strOneChar[i];
+        arrDotAndDash.push(q.replace(/0{2}/g, '').replace(/10/g, '.').replace(/11/g, '-'));
+    }
+
+    for (let i = 0; i < arrDotAndDash.length; i++) {
+        let q = arrDotAndDash[i];
+
+        for (let key in MORSE_TABLE) { // получить значение по ключу
+            if (key == q) { arrWords.push(MORSE_TABLE[key])}
+        }
+    }
+    
+    return arrWords.join``;
 }
 
 module.exports = {
